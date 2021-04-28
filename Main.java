034cc28cbc11a07473ec2b4ce2014569822e7c71
lasisi Ezekiel <ezekielakintunde18@gmail.com>
2021-04-28 05:48:16 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Animals animal = new Animals("dan",1, 1, 5, 5);
+
+        Dog dog = new Dog("bingo", 34, 90, 2, 4, 1, 34, "brown cow");
+
+
+    }
+
+}
+
